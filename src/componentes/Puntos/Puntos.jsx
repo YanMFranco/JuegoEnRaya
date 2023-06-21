f124059204cd64b28309589339b41c937b3674e9
YanMFranco <106338421+YanMFranco@users.noreply.github.com>
2023-06-20 20:38:23 -0500

    Descripción de los cambios

--- src/componentes/Puntos/Puntos.jsx
+++ src/componentes/Puntos/Puntos.jsx
@@ -3,8 +3,10 @@ import './Puntos.css';
 const Puntos = ({puntosX,puntosO}) =>{
     return (
         <div className='puntos'>
+
             <div>{puntosX}</div>
             <div>{puntosO}</div>
+            
         </div>
     )
 }
